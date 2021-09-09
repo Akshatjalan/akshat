a7422b1856a19fe95e9aa5dc5cdf98c161b66171
Akshat Jalan <61394548+Akshatjalan@users.noreply.github.com>
2021-09-10 01:34:03 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,24 +227,6 @@
   });
 
   /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
    * Animation on scroll
    */
   window.addEventListener('load', () => {
